82acb94feacd5ac3b1d7d6ec34a6f66d473ef74e
Mariia Berger <slackerpanda@gmail.com>
2021-07-31 17:39:08 +0300

    create 3 view controllers: get started, choose category, choose learning way

---
+++ EnglishBook/Controllers/GetStartedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GetStartedViewController.swift
+//  EnglishBook
+//
+//  Created by Maria Berger on 31.07.2021.
+//
+
+import UIKit
+
+class GetStartedViewController: UIViewController {
+
+    @IBOutlet weak var getStartedButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        getStartedButton.layer.cornerRadius = 20
+        
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      let _ = segue.destination as? ChooseCategoryViewController
+    }
+}
---
+++ EnglishBook/Controllers/LearningWayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LearningWayViewController.swift
+//  EnglishBook
+//
+//  Created by Maria Berger on 31.07.2021.
+//
+
+import UIKit
+
+class LearningWayViewController: UIViewController {
+
+    @IBOutlet weak var dictionaryButton: UIButton!
+    @IBOutlet weak var fillInTheBlanksButton: UIButton!
+    @IBOutlet weak var checkYourself: UIButton!
+    
+
+    
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}
+
