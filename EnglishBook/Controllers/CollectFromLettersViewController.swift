09fd19b5d5fbe375fae2590c40b1c9ef26c9408e
Sergey <rumyantsev.serge@yandex.ru>
2021-08-01 10:37:04 +0300

    CollectFromLetters initial commit

---
+++ EnglishBook/Controllers/CollectFromLettersViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CollectFromLettersViewController.swift
+//  EnglishBook
+//
+//  Created by Sergey on 01.08.2021.
+//
+
+import UIKit
+
+class CollectFromLettersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
